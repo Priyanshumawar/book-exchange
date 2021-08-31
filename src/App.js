4200671b3ab0d0bc6f39eea76c6d1a814ac0b98a
Priyanshumawar <85007965+Priyanshumawar@users.noreply.github.com>
2021-08-31 17:39:26 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import Header from "./components/header"
+import "./components/style.css"
+import Footer from "./components/footer"
+
+
+function App() {
+  return (
+    <>
+      <Header/> 
+      <Footer/>
+      
+    </>
+       
+  );
+}
+
+export default App;
